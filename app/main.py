10621335e915173d290513c2f3ba4394ce44f329
Björn <bjoern@familie-engel.info>
2026-03-11 11:06:45 +0100

    Exception

--- app/main.py
+++ app/main.py
@@ -1,22 +1,29 @@
+import logging
 import time
 from mqtt_service import MQTTService
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("Main")
 
 # Modules
 from modules.ecowater_module import EcowaterModule
 from modules.myvaillant_module import MyVaillantModule
 from modules.uponor_module import UponorModule
 
-mqtt = MQTTService()
-mqtt.start()
+try:
+    mqtt = MQTTService()
+    mqtt.start()
 
-modules = [
-    EcowaterModule(mqtt),
-    MyVaillantModule(mqtt),
-    UponorModule(mqtt),
-]
+    modules = [
+        EcowaterModule(mqtt),
+        MyVaillantModule(mqtt),
+        UponorModule(mqtt),
+    ]
 
-for module in modules:
-    module.start()
+    for module in modules:
+        module.start()
 
-while True:
-    time.sleep(60)
+    while True:
+        time.sleep(60)
+except Exception as e:
+    logger.exception(e)
